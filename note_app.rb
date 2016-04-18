52c806a2c7cfe5d76fdbaaf6dcfaedff217e479b
Jesusbaby <olayemifolakemi@yahoo.com>
2016-04-18 12:48:39 +0100

    The note app with required specifications for create note feature

--- note_app.rb
+++ note_app.rb
@@ -1,51 +1,57 @@
-require 'firebase'
-require 'sqlite3'
-$db = SQLite3::Database.open "note.db"
-module Eazynote
-  # create a class note-taking application that uses modules for namespacing
-  class NotesApplication
-    attr_reader :name, :notes
 
-    def initialize(author)
-      error_handler author
-      @name = author
-      @notes = $db.execute("SELECT * FROM my_notes")
-    end
+module EasyNote
+  class CreateNote
+	def initialize
+	  @title = getTitle
+	  @speaker = getSpeakerName
+	  @note_date = setDate
+	  @content = getContent
+	  saveNote
+	end
 
-    def create(note_content)
-      check_note_input note_content
-      sql = "INSERT INTO my_notes (`note_content`) VALUES ('#{note_content}')"
-	  $db.execute(sql)
-    end
+	def printNote
+	  puts @title
+	  puts @content
+	  puts @speaker
+	  puts @note_date
+	end
 
-    def get(note_id)
-      check_note_id note_id
-      @notes[note_id]
-    end
+	private
+	
+	def getTitle
+	  puts "Note title: "
+	  gets.chomp
+	end
+
+	def getSpeakerName
+	  puts "Speaker:"
+	  gets.chomp
+	end
+
+	def setDate
+	  time = Time.now
+	end
+
+	def getContent
+	  puts "Note content:"
+	  gets.chomp
+	end
+
+	def saveNote
+	  file = File.new(@title<<".txt", "w+")
+	  file.syswrite "Title: " << @title
+	  file.syswrite "\n"
+	  file.syswrite "Speaker: "<< @speaker
+	  file.syswrite "\n"
+	  file.syswrite "Date: "
+	  file.syswrite @note_date
+	  file.syswrite "\n"
+	  file.syswrite "Content: "<< @content
+	  file.syswrite "\n"
+	  file.close
+	end
   
-    private
-
-    def check_if_note_exists(note_id)
-      return 'Note does not exist' if @notes[note_id].nil?
-      nil
-    end
-
-    def check_note_input(note)
-      raise 'Input should be a string' unless note.is_a? String
-      nil
-    end
-
-    def check_note_id(note_id)
-      raise 'Note Id must be an Integer' unless note_id.is_a? Integer
-      nil
-    end
-
-    def error_handler(author)
-      raise 'Input should be a string' unless author.is_a? String
-      raise 'No Chars Allowed' if author =~ /[~!@#$%^&*(){}|:<>]+/
-      raise 'Author name cannot be empty' if author.empty?
-      raise RuntimeError if author.nil?
-      nil
-    end
   end
+
 end
+EasyNote::CreateNote.new()
