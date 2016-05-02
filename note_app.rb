28d232e7462a2273ab6f2a37781d49175a4b2390
Jesusbaby <olayemifolakemi@yahoo.com>
2016-05-02 15:59:04 +0100

    tested with rubocop

--- note_app.rb
+++ note_app.rb
@@ -1,48 +1,40 @@
 module EasyNote
   # create a class for the "createnote" feature within the Easynote Namespace
   class CreateNote
-  	@@date = Time.now.strftime("%m/%d/%Y")
-  	@@time = Time.now.strftime("%I:%M%p")
-
-  	
     def initialize
-	  @title = note_title
-	  @speaker = note_speaker_name
-	  @content = note_content
-	  save_note
-	end
+      @title = note_title
+      @speaker = note_speaker_name
+      @content = note_content
+      save_note
+    end
 
     private
-	
+
     def note_title
-	  puts 'Note title: '
-	  gets.chomp
-	end
+      puts 'Note title: '
+      gets.chomp
+    end
 
     def note_speaker_name
-	  puts "Speaker:"
-	  gets.chomp
+      puts 'Speaker:'
+      gets.chomp
     end
 
     def note_content
-	  puts "Note content:"
-	  gets.chomp
-	end
+      puts 'Note content:'
+      gets.chomp
+    end
 
     def save_note
-	  file = File.new(@title << " .txt", "w+")
-	  file.syswrite 'Title: ' << @title
-	  file.syswrite "\n"
-	  file.syswrite 'Speaker: ' << @speaker
-	  file.syswrite "\n"
-	  file.syswrite 'Date: ' << @@date
-	  file.syswrite "\n"
-	  file.syswrite 'Time: ' << @@time
-	  file.syswrite "\n"
-	  file.syswrite 'Content: ' << @content
-	  file.syswrite "\n"
-	  file.close
-	end
+      c = 'Title: ' << @title << "\n"
+      c += 'Speaker: ' << @speaker << "\n"
+      c += 'Date: ' << Time.now.strftime('%m/%d/%Y') << "\n"
+      c += 'Time: ' << Time.now.strftime('%I:%M%p') << "\n"
+      c += 'Content: ' << @content << "\n"
+      file = File.new(@title << '.txt', 'w+')
+      file.syswrite c
+      file.close
+    end
   end
 end
-EasyNote::CreateNote.new()
+EasyNote::CreateNote.new
